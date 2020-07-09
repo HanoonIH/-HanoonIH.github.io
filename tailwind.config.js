9b4e7923ecdc3f311a75c32decbc95c5b74b3e62
HanoonIH <hanoonih123@gmail.com>
2020-07-09 23:45:29 +0530

    1st, made navbar and hero section basic, 9/7/2020

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      display: ['Bree Serif', 'serif'],
+      body: ['Roboto', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
